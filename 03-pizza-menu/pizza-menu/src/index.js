9db0b4af99c701141cfab2dad47b19c05bea1faa
Warren Walters <warren.salesforce@gmail.com>
2024-09-13 16:33:24 -0500

    MOved started code

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello World!</h1>;
+    return <h1>Hello World!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
