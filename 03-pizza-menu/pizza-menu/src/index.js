a2d741c0675580ca2e857a374ada013b04ee59cc
Warren Walters <warren.salesforce@gmail.com>
2024-09-15 13:28:53 -0500

    footer updates

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello World!</h1>;
+    return (
+        <div>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,3 +17,39 @@ root.render(
         <App />
     </React.StrictMode>
 );
+
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/focaccia.jpg" alt="Focaccia" />
+            <h2>Focaccia</h2>
+            <p>Bread with italian olive oil and rosemary</p>
+        </div>
+    );
+}
+
+function Header() {
+    return <h1>Fast React Pizza Company</h1>;
+}
+
+function Menu() {
+    return (
+        <div>
+            <h2>Menu</h2>
+            <Pizza />
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+
+    return (
+        <footer>
+            {new Date().toLocaleTimeString()}. &copy; 2021 Fast React Pizza
+            Company
+        </footer>
+    );
+}
