ea95ea500d848f38203858a79c7481776ceca96f
Warren Walters <warren.salesforce@gmail.com>
2024-09-18 16:50:06 -0500

    Working static page. Pending step update

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,5 +1,36 @@
-function App() {
-    return <h1>Hello World</h1>;
-}
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
 
-export default App;
+export default function App() {
+    let step = 1;
+    return (
+        <div className="steps">
+            <div className="numbers">
+                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+            </div>
+
+            <p className="message">
+                Step {step}: {messages[step - 1]}
+            </p>
+            <div className="buttons">
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={() => step--}
+                >
+                    Previous
+                </button>
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={() => step++}
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
