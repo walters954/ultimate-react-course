f048c7c8cf538e43c7bc65b964de8df524f32c0c
Warren Walters <warren.salesforce@gmail.com>
2024-09-20 17:49:44 -0500

    counter v2 with range and input

--- 04-steps/counter/src/App.js
+++ 04-steps/counter/src/App.js
@@ -15,29 +15,36 @@ function Counter() {
     const date = new Date();
     date.setDate(date.getDate() + count);
 
-    function handleStepIncrease() {
-        if (step > 0) setStep((s) => s + 1);
-    }
-
-    function handleStepDecrease() {
-        if (step > 1) setStep((s) => s - 1);
+    function handleReset() {
+        setCount(0);
+        setStep(1);
     }
 
     return (
         <>
             <div>
-                <button onClick={handleStepDecrease}>-</button>
+                <input
+                    type="range"
+                    min="1"
+                    max="10"
+                    value={step}
+                    onChange={(e) => setStep(Number(e.target.value))}
+                />
                 <span>Steps : {step}</span>
-                <button onClick={handleStepIncrease}>+</button>
             </div>
             <div>
                 <button onClick={() => setCount((c) => c - step)}>-</button>
-                <span>Count : {count}</span>
+                <input
+                    type="text"
+                    value={count}
+                    onChange={(e) => setCount(Number(e.target.value))}
+                />
                 <button onClick={() => setCount((c) => c + step)}>+</button>
             </div>
             <div>
                 {count} days from today is {date.toDateString()}
             </div>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 }
