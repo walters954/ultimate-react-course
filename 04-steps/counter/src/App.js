c04c908f888ea48bc03a91449387a840e88edb06
Warren Walters <warren.salesforce@gmail.com>
2024-09-20 16:04:06 -0500

    working date counter

---
+++ 04-steps/counter/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function App() {
+    return (
+        <div>
+            <DateCounter></DateCounter>
+        </div>
+    );
+}
+
+function DateCounter() {
+    const [step, setStep] = useState(1);
+    const [count, setCount] = useState(0);
+
+    const date = new Date();
+    date.setDate(date.getDate() + count);
+
+    function handleStepIncrease() {
+        if (step > 0) setStep((s) => s + 1);
+    }
+
+    function handleStepDecrease() {
+        if (step > 1) setStep((s) => s - 1);
+    }
+
+    return (
+        <>
+            <div>
+                <button onClick={handleStepDecrease}>-</button>
+                <span>Steps : {step}</span>
+                <button onClick={handleStepIncrease}>+</button>
+            </div>
+            <div>
+                <button onClick={() => setCount((c) => c - step)}>-</button>
+                <span>Count : {count}</span>
+                <button onClick={() => setCount((c) => c + step)}>+</button>
+            </div>
+            <div>
+                {count} days from today is {date.toDateString()}
+            </div>
+        </>
+    );
+}
