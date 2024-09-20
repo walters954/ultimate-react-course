10f1d2cfebc833848d3fb0cf1dbc600bac56f2ea
Warren Walters <warren.salesforce@gmail.com>
2024-09-20 16:43:49 -0500

    form configuured

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,11 +1,17 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 export default function App() {
     return (
-        <>
+        <div className="app">
             <Logo />
             <Form />
             <PackingList />
             <Stats />
-        </>
+        </div>
     );
 }
 
@@ -14,15 +20,47 @@ function Logo() {
 }
 
 function Form() {
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
             <h3>What do you need for your 😍 trip?</h3>
-        </div>
+            <select>
+                {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+                    <option value={num} key={num}>
+                        {num}
+                    </option>
+                ))}
+            </select>
+            <input type="text" placeholder="Item..." />
+            <button>Add</button>
+        </form>
     );
 }
 
 function PackingList() {
-    return <div className="list">List</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} key={item.id}></Item>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
 }
 
 function Stats() {
