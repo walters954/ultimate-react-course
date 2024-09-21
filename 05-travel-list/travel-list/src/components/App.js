d14cd7fda751f6582710c745c42a863da7f3e6fa
Warren Walters <warren.salesforce@gmail.com>
2024-09-21 11:59:47 -0500

    default import and components folder

--- 05-travel-list/travel-list/src/components/App.js
+++ 05-travel-list/travel-list/src/components/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
-import Form from "./components/Form";
+import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 const initialItems = [
