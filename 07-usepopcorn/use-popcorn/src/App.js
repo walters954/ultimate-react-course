469adcad83617b6447e633415f023b77f4a62410
Warren Walters <warren.salesforce@gmail.com>
2024-09-22 22:41:25 -0500

    working on star rating

--- 07-usepopcorn/use-popcorn/src/App.js
+++ 07-usepopcorn/use-popcorn/src/App.js
@@ -83,18 +83,18 @@ function Main({ children }) {
     return <main className="main">{children}</main>;
 }
 
-function MovieListBox({ children }) {
-    const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
         <div className="box">
             <button
                 className="btn-toggle"
-                onClick={() => setIsOpen1((open) => !open)}
+                onClick={() => setIsOpen((open) => !open)}
             >
-                {isOpen1 ? "–" : "+"}
+                {isOpen ? "–" : "+"}
             </button>
-            {isOpen1 && children}
+            {isOpen && children}
         </div>
     );
 }
@@ -121,28 +121,6 @@ function Movie({ movie }) {
                 </p>
             </div>
         </li>
-    );
-}
-
-function WatchedMoviesListBox() {
-    const [isOpen2, setIsOpen2] = useState(true);
-    const [watched, setWatched] = useState(tempWatchedData);
-
-    return (
-        <div className="box">
-            <button
-                className="btn-toggle"
-                onClick={() => setIsOpen2((open) => !open)}
-            >
-                {isOpen2 ? "–" : "+"}
-            </button>
-            {isOpen2 && (
-                <>
-                    <WatchedMovieSummary watched={watched} />
-                    <WatchedMovieList watched={watched} />
-                </>
-            )}
-        </div>
     );
 }
 
@@ -210,6 +188,8 @@ function WatchedMovie({ movie }) {
 
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
+
     return (
         <>
             <NavBar>
@@ -218,10 +198,13 @@ export default function App() {
                 <NumResults movies={movies} />
             </NavBar>
             <Main>
-                <MovieListBox>
+                <Box>
                     <MovieList movies={movies} />
-                </MovieListBox>
-                <WatchedMoviesListBox />
+                </Box>
+                <Box>
+                    <WatchedMovieSummary watched={watched} />
+                    <WatchedMovieList watched={watched} />
+                </Box>
             </Main>
         </>
     );
