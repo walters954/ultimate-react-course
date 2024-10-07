e47f4f3026ad5f092baeeabc22eb0d2af0ec9831
Warren Walters <warren.salesforce@gmail.com>
2024-10-07 17:12:11 -0500

    Rating with messages

--- 07-usepopcorn/use-popcorn/src/index.js
+++ 07-usepopcorn/use-popcorn/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         {/* <App /> */}
+        <StarRating
+            maxRating={5}
+            message={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+        />
         <StarRating maxRating={5} />
     </React.StrictMode>
 );
