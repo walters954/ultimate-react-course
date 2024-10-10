d4ca32482485a2cd11dd05ea69c76754e56d8f72
Warren Walters <warren.salesforce@gmail.com>
2024-10-10 00:01:14 -0500

    HANDLE UNDO

--- 08-how-react-works/how-react-works/src/App.js
+++ 08-how-react-works/how-react-works/src/App.js
@@ -66,6 +66,15 @@ function TabContent({ item }) {
         setLikes(likes + 1);
     }
 
+    function handleUndo() {
+        setLikes(0);
+        setShowDetails(true);
+    }
+
+    function handleUndoLater() {
+        setTimeout(handleUndo, 2000);
+    }
+
     return (
         <div className="tab-content">
             <h4>{item.summary}</h4>
@@ -84,8 +93,8 @@ function TabContent({ item }) {
             </div>
 
             <div className="tab-undo">
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleUndo}>Undo</button>
+                <button onClick={handleUndoLater}>Undo in 2s</button>
             </div>
         </div>
     );
